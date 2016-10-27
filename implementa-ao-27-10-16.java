c49775f2e458afef52afb8ea3959564c157e19c3
willian2016a <willianalvesalmeida@yahoo.com.br>
2016-10-27 11:59:12 -0200

    Create implementa-ao-27-10-16.java

---
+++ implementa-ao-27-10-16.java
@@ -0,0 +1,8 @@
+
+
+
+<div id="divExclusao" style="display:none">
+				   <h5>Deseja realmente excluir esta turma? <h5>
+				   <a href = 'deleteTurma.php?id=<?php echo $Turma['idTurma']?>'>Sim</a>
+				   <a href="#" onClick="$('#divExclusao').hide();">Não</a>
+</div>
